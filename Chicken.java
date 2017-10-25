70b9b63945f0124ae43eb5af30f3444a6ba7e33d
jjtorres9 <jjtorres9@espe.edu.ec>
2017-10-25 11:46:21 -0500

    version 1

---
+++ Chicken.java
@@ -0,0 +1,23 @@
+
+public class Chicken {
+	private string name;
+	private string color;
+	private string age;
+	public void layAnnEgg(){
+		System.out.println ("the chicken lay ann egg");
+	}
+	public void poop(){
+	System.out.println ("the chicken pooped");}
+	public void drink(){
+ System.out.println ("the chicken drank");
+	}
+		public void print(){
+			System.out.println ("the name of the chicken is  -->"+ name+ "color" + color +  "ege"+ age );
+		}
+	public void initializeData (){
+		this.name=name;
+		this.color=color;
+		this.age=age;
+	}
+	
+}
--- helloworld.java
+++ helloworld.java
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
+//helloword.java
+public class HellowWorld{
+public static	void main(String [] args){
+	System.out.println ("hellow  jhon torres");
+	chicken = new Chicken();
+	chicken.initialize("lucy","Brown","2");
+	chicken.print();
+}
+=======
     public class HolaMundo {
      
     	public static void main(String[] args) {		
@@ -5,3 +15,4 @@
     	}
      
     }
+>>>>>>> dfda1cbd4c2526b84874075f3b4e965ab410e4a0
