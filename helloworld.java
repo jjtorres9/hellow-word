6f918280c0d9e14cdb261d2ef4adc3b0b0274626
jjtorres9 <jjtorres9@espe.edu.ec>
2017-10-25 23:15:14 -0500

    version 2

--- helloworld.java
+++ helloworld.java
@@ -1,18 +1,29 @@
-<<<<<<< HEAD
-//helloword.java
-public class HellowWorld{
-public static	void main(String [] args){
-	System.out.println ("hellow  jhon torres");
-	chicken = new Chicken();
-	chicken.initialize("lucy","Brown","2");
-	chicken.print();
+public class helloworld{
+	public static void main(String[] args){
+		System.out.println("Hello JHON TORRES");
+	
+	chicken chicken1;
+	chicken1=new chicken();
+	chicken chickenone;
+	chickenone= new chicken ();
+	chicken chickentwo;
+	chickentwo= new chicken ();
+	chicken chickentree;
+	chickentree= new chicken ();
+	chicken1.initializeData("Lucy","Brown",2);
+	chickentree.initializeData("rosa","black and white",1);
+	chickentwo.initializeData("florencia"," white",1);
+	chickenone.initializeData("martina","black",3);
+	chicken1.print();
+	chickenone.print ();
+	chickentwo.print ();
+	chickentree.print();
+	chicken1.drink ();
+	chicken1.poop ();
+	chicken1.layAnEgg();
+	chickenone.drink ();
+	chickentwo.poop ();
+	chickentree.layAnEgg ();
+	}
+	
 }
-=======
-    public class HolaMundo {
-     
-    	public static void main(String[] args) {		
-    		System.out.println("Hola Mundo");
-    	}
-     
-    }
->>>>>>> dfda1cbd4c2526b84874075f3b4e965ab410e4a0
